34fd39473f789fba072197908634cdb7851916cc
Shubham Anil Kakade <skakade2@charlotte.edu>
2025-10-17 00:11:51 -0400

    config updated after hosting

--- src/config.cpp
+++ src/config.cpp
@@ -1,6 +1,6 @@
 #include "config.h"
 
 std::string CONFIG::client_username;
-const std::string CONFIG::SERVER_IP = "0.0.0.0";
-const unsigned short CONFIG::SERVER_PORT_API = 0000;
-const unsigned short CONFIG::SERVER_PORT_SOCKET = 0000;
+const std::string CONFIG::SERVER_IP = "34.31.74.39";
+const unsigned short CONFIG::SERVER_PORT_API = 5000;
+const unsigned short CONFIG::SERVER_PORT_SOCKET = 9000;
